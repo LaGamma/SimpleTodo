0c101c8ec341f193561ea7f2304d4a56deed3c38
LaGamma <nplagamma@email.wm.edu>
2020-09-05 18:34:32 -0400

    edit

--- app/src/main/java/com/nicolaslagamma/simpletodo/MainActivity.java
+++ app/src/main/java/com/nicolaslagamma/simpletodo/MainActivity.java
@@ -91,7 +91,7 @@ public class MainActivity extends AppCompatActivity {
 
     // Handle the result of the edit activity
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if (resultCode == RESULT_OK && requestCode == EDIT_TEXT_CODE) {
             // Retrieve the update text value
