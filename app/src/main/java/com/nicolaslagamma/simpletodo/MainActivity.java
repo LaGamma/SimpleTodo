7bd0024ce7d3f85d809a4cc45c1e820173717d98
LaGamma <nplagamma@email.wm.edu>
2020-09-09 15:28:31 -0400

    fix

--- app/src/main/java/com/nicolaslagamma/simpletodo/MainActivity.java
+++ app/src/main/java/com/nicolaslagamma/simpletodo/MainActivity.java
@@ -1,6 +1,5 @@
 package com.nicolaslagamma.simpletodo;
 
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
